91c62c6b991eefd06ac036c1b16a7a1f03e3bf40
mohiyaddeen7 <mohiyaddeenraza7@gmail.com>
2023-11-16 08:47:40 +0530

    converted promises to async await in db.js

--- backend/db.js
+++ backend/db.js
@@ -3,16 +3,18 @@ require("dotenv").config();
 const mongoURI = process.env.MONGODB_URI
 
 const connetToMongo = async () => {
-  mongoose.connect(mongoURI)
-    .then(() => {
-      console.log("connected to MongoDB successfully");
-    })
-    .catch((err) => {
-      console.log({
-        error: `Error connecting to Database`,
-        message: `${err.message}`,
-      });
+
+  try {
+    await mongoose.connect(mongoURI)
+    console.log("connected to MongoDB successfully");
+  } catch (error) {
+    console.log({
+      error: `Error connecting to Database`,
+      message: `${err.message}`,
     });
+  }
+  
+  
 };
 
 module.exports = connetToMongo;
